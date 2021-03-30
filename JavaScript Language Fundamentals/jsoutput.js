0110a6c452389b41fa87657f5f3f87293cc5219f
PURVA TAYDE <purvatayde1908@gmail.com>
2021-03-30 13:18:30 +0530

    fund

---
+++ JavaScript Language Fundamentals/jsoutput.js
@@ -0,0 +1,21 @@
+// external js file
+// Write all JavaScript code here
+
+alert('I am Dinanath Jayaswal');
+window.alert('We are learning JavaScript');
+
+console.log('I am Dinanath Jayaswal');
+console.log('We are learning JavaScript');
+
+// external js file
+// Write all JavaScript code here
+
+document.write('<hr/>');
+document.write('I am Dinanath Jayaswal');
+document.write('<h1>We are learning JavaScript</h1>');
+
+document.getElementById('mainHeadingText').innerHTML = 'Heading Text change dynamically on run-time';
+
+var paraText = document.getElementById('mainParaText');
+paraText.innerHTML = '<strong>This Paragraphic text inserted dynamically through innerHTML method.</strong>';
+
